1f481a871ab8c315bd507d638430c9847b00b092
JorgeSotoAudelo <115675512+JorgeSotoAudelo@users.noreply.github.com>
2023-06-23 21:19:54 +0000

    Modificando rutas de imagenes y estilos para el servidor

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
 
-app = Flask('app',template_folder='app/vistas')
+app = Flask('app',template_folder='app/vistas',static_folder='app/static')
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/registro')
+def registro():
+    return render_template('registro.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
