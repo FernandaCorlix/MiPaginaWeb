2c92dc517d1ed25f1236965954579de64468f782
JorgeSotoAudelo <115675512+JorgeSotoAudelo@users.noreply.github.com>
2023-08-10 04:35:05 +0000

    Se gestionan los equipos

--- app.py
+++ app.py
@@ -83,6 +83,61 @@ def borrarEquipo(equipo_id):
             conexion.close()
     return redirect('/listaDeEquipos')
 
+@app.route('/listaDeEquipos/consultar/<int:equipo_id>')
+def consultarEquipo(equipo_id):
+    conexion =connectionPool.get_connection()
+    try:
+        cursor = conexion.cursor()
+        sqlJugadores = "Select * from jugadores where EquipoID = (%s)"
+        cursor.execute(sqlJugadores,(equipo_id,))
+        listaJugadores = cursor.fetchall()
+        sqlEquipo = "Select nombreEquipo from Equipo where EquipoID = (%s)"
+        cursor.execute(sqlEquipo,(equipo_id,))
+        nombreEquipo = cursor.fetchone()[0]
+        sqlJugadoresDisponibles = "Select * from jugadores where EquipoID != (%s) OR EquipoID IS NULL;"
+        cursor.execute(sqlJugadoresDisponibles,(equipo_id,))
+        jugadoresDisponibles = cursor.fetchall()
+        print("equipo_id:", equipo_id)
+
+
+    finally:
+        if conexion:
+            conexion.close()
+    return render_template('consultaEquipo.html',listaJugadores = listaJugadores, nombreEquipo = nombreEquipo, equipo_id = int(equipo_id),jugadoresDisponibles = jugadoresDisponibles)
+
+@app.route('/listaDeEquipos/agregarJugadores/<int:equipo_id>',methods = ['POST'])
+def agregarJugadores(equipo_id):
+    conexion = connectionPool.get_connection()
+    listaIDsJugadores = request.form.getlist('jugadores_seleccionados')
+    if len(listaIDsJugadores) > 0:
+        cond = ""
+        if len(listaIDsJugadores) == 1:
+            cond = f"({listaIDsJugadores[0]})"
+        else:
+            cond = tuple(listaIDsJugadores)
+        try:
+            cursor = conexion.cursor()
+            sql = f'UPDATE jugadores SET EquipoID = %s WHERE id IN {cond}'
+            print(f"SQL: {sql}")
+            cursor.execute(sql,(equipo_id,))
+            conexion.commit()
+        finally:
+            if conexion:
+                conexion.close()
+    return redirect(f'/listaDeEquipos/consultar/{equipo_id}')
+
+@app.route('/listaDeEquipos/borrarEquipoJugador/<int:jugador_id>',methods = ['POST'])
+def borrarEquipoJugador(jugador_id):
+    conexion = connectionPool.get_connection()
+    try:
+        cursor = conexion.cursor()
+        sql = f'UPDATE jugadores set EquipoID = NULL WHERE id = %s'
+        cursor.execute(sql,(jugador_id,))
+        conexion.commit()
+    finally:
+        if conexion:
+            conexion.close()
+    return redirect(request.referrer or '/listaDeEquipos')    
 @app.route('/crearEquipo', methods=['GET', 'POST'])
 def crearEquipo():
     if request.method == 'GET':
